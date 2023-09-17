310dc87f9c0d03c5d40e3705936e9e3dd8313a6f
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-09-17 11:58:06 +0700

    fix: remove redundant console.log

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-console.log()
 app.use('/public', express.static(path.join(__dirname, '../frontend')));
 
 // Delegate routing to the frontend
