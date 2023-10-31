cb5ee33377d5867a1b09b257f91db6d02db58e87
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-10-31 23:51:21 +0700

    doc: fix jsdoc

--- backend/utils/logger/logPrinterJob.js
+++ backend/utils/logger/logPrinterJob.js
@@ -5,15 +5,16 @@ const prisma = new PrismaClient();
 /**
  * Log a printer job into the database
  * @param {
- *  { printerId: string,
- *    userId: string,
- *    fileId: string,
- *    status: PrinterJobStatus,
- *    estimatedTime: number,
- *    createdAt: number,
- *    copiesNo: number,
- *    startPage: number,
- *    endPage: number } } printerJobInfo - Information about the printer job
+    { printerId: string,
+      userId: string,
+      fileId: string,
+      status: PrinterJobStatus,
+      estimatedTime: number,
+      createdAt: number,
+      copiesNo: number,
+      startPage: number,
+      endPage: number }
+    } printerJobInfo - Information about the printer job
  * @returns { { success: boolean, error?: any } } - whether the logging was successful
  */
 export default async function logPrinterJob(printerJobInfo) {
--- backend/utils/logger/logTransaction.js
+++ backend/utils/logger/logTransaction.js
@@ -5,12 +5,12 @@ const prisma = new PrismaClient();
 /**
  * Log the transaction into the database
  * @param {
- *  { userId: string,
- *    paperNo: number,
- *    price: number,
- *    createdAt: string,
- *    success: boolean }
- * } transaction - information about the transaction
+    { userId: string,
+      paperNo: number,
+      price: number,
+      createdAt: string,
+      success: boolean }
+    } transaction - information about the transaction
  * @returns { { success: boolean, error?: any } } - whether the logging was successful
  */
 export default async function logTransaction(transaction) {
