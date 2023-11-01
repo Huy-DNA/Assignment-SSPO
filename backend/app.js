3eba7225f9530db51c50cd0d286445b8dee264dd
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-01 11:28:03 +0700

    chore: rename plugins to routers

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,8 @@ import logger from 'morgan';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import LoginPlugin from './plugins/login.js';
-import LogoutPlugin from './plugins/logout.js';
+import LoginRouter from './routers/login.js';
+import LogoutRouter from './routers/logout.js';
 import API from './api/index.js';
 
 dotenv.config();
@@ -30,8 +30,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, '../frontend')));
 
-app.use(LoginPlugin);
-app.use(LogoutPlugin);
+app.use(LoginRouter);
+app.use(LogoutRouter);
 app.use('/api', API);
 
 // Delegate routing to the frontend
