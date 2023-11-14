7989860e504e646c5d0008f408a6d3f30c4dff44
havanchau <chauhvc2.dy@gmail.com>
2023-11-14 20:18:34 +0700

    add update

--- frontend/src/components/DisplayPrinter/DisplayPrinter.jsx
+++ frontend/src/components/DisplayPrinter/DisplayPrinter.jsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
 import {
@@ -19,55 +18,26 @@ import {
 function EditToolbar(props) {
   const { setRows, setRowModesModel, checkIDs, setCheckIDs } = props;
 
-  // const handleAddPrinters = () => {
-  //   const id = "";
-  //   setRows((oldRows) => [...oldRows, {id, name: '', location: '', enabled: true }]);
-  //   setRowModesModel((oldModel) => ({
-  //     ...oldModel,
-  //     [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
-  //   }));
-  // };
-  const handleAddPrinters = () => {
-    // Tạo một hàng mới mà không cần khởi tạo trường id
-    const newPrinter = { name: '', location: '', enabled: true };
-
-    // Thêm hàng mới vào grid
-    setRows((oldRows) => [...oldRows, newPrinter]);
-
-    // Cập nhật trạng thái rowModesModel để chuyển hàng mới vào chế độ chỉnh sửa
-    const newId = uuidv4();
+  const handleAddRow = () => {
+    const id = '';
+    setRows((oldRows) => [...oldRows, {id, name: '', location: '', enabled: true }]);
     setRowModesModel((oldModel) => ({
       ...oldModel,
-      [newId]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
     }));
-
-    console.log(newPrinter);
-
-    // Gọi API để thêm máy in với dữ liệu từ hàng mới
-    // axios({
-    //   method: 'post',
-    //   url: 'http://localhost:3000/api/printers/add',
-    //   data: newPrinter,
-    // })
-    //   .then((response) => {
-    //     // Cập nhật id của hàng sau khi nhận phản hồi từ máy chủ
-    //     const { id } = response.data;
-    //     const updatedRows = oldRows.map((row) =>
-    //       row === newPrinter ? { ...row, id } : row
-    //     );
-    //     setRows(updatedRows);
-    //   })
-    //   .catch((error) => {
-    //     // Xử lý lỗi khi gọi API thêm máy in
-    //     console.error('Error adding printer:', error);
-    //     // Nếu có lỗi, hãy xóa hàng mới từ grid
-    //     setRows((oldRows) => oldRows.filter((row) => row !== newPrinter));
-    //     // Nếu có lỗi, hãy xóa hàng mới từ rowModesModel
-    //     setRowModesModel((oldModel) => {
-    //       const { [newId]: deleted, ...newModel } = oldModel;
-    //       return newModel;
-    //     });
-    //   });
+  };
+  const handleAddPrinters = (data) => {
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/printers/add',
+      data: data,
+    })
+      .then((response) => {
+        
+      })
+      .catch((error) => {
+        
+      });
   };
 
   const handleDeletePrinters = () => {
@@ -90,12 +60,12 @@ function EditToolbar(props) {
   return (
     <div style={{ display: 'flex' }}>
       <GridToolbarContainer>
-        <Button color="primary" startIcon={<AddIcon />} onClick={handleAddPrinters}>
+        <Button color="primary" startIcon={<AddIcon />} onClick={handleAddRow}>
           Thêm máy in
         </Button>
       </GridToolbarContainer>
       <GridToolbarContainer>
-        <Button color="primary" startIcon={<AddIcon />} onClick={handleDeletePrinters}>
+        <Button color="primary" startIcon={<RemoveCircleOutlineIcon />} onClick={handleDeletePrinters}>
           Xoá máy in
         </Button>
       </GridToolbarContainer>
@@ -131,10 +101,6 @@ export default function FullFeaturedCrudGrid() {
 
   const handleSaveClick = (id) => () => {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-  };
-
-  const handleDeleteClick = (id) => () => {
-    setRows(rows.filter((row) => row.id !== id));
   };
 
   const handleCancelClick = (id) => () => {
@@ -214,13 +180,7 @@ export default function FullFeaturedCrudGrid() {
             className="textPrimary"
             onClick={handleEditClick(id)}
             color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
+          />
         ];
       },
     },
--- frontend/src/pages/PrinterManagement/PrinterManagement.jsx
+++ frontend/src/pages/PrinterManagement/PrinterManagement.jsx
@@ -1,64 +1,16 @@
-import axios from 'axios';
 import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faMagnifyingGlass,
-  faAngleLeft,
-  faAngleRight,
 } from '@fortawesome/free-solid-svg-icons';
-import {
-  faCircleXmark,
-} from '@fortawesome/free-regular-svg-icons';
 
 import styles from './PrinterManagement.module.scss';
-import GetPrinters, { deletePrinters, editPrinters, displayPrinter } from '../../components/GetPrinters/GetPrinters';
 import DisplayPrinter from '../../components/DisplayPrinter/DisplayPrinter';
 
 const cx = classNames.bind(styles);
 
 function PrinterManagement() {
-  const [printers, setPrinters] = useState([]);
-  const [checkedIds, setCheckedIds] = useState([]);
-  const [recallRender, setRecallRender] = useState(false);
-  const [isEditPrinters, setIsEditPrinters] = useState(false);
-  // Define URL for API Printer
-  const getPrinterURL = {
-    URL: 'http://localhost:3000/api/printers',
-    method: 'get',
-  };
-
-  const deletePrinterURL = {
-    URL: 'http://localhost:3000/api/printers/delete',
-    method: 'post',
-  };
-
-  const handleEditPrinter = () => {
-    setIsEditPrinters(true);
-  }
-
-  const handleSavePrinter = () => {
-    setIsEditPrinters(false);
-  }
-
-  const handleButtonClick = () => {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = false;
-      setCheckedIds([]);
-    });
-  };
-
-  // Push id checkbox into Array checkedIds
-  const handleCheckboxGetID = (e) => {
-    const id = e.target.id;
-    if (e.target.checked) {
-      setCheckedIds([...checkedIds, id]);
-    } else {
-      const updatedIds = checkedIds.filter((checkedId) => checkedId !== id);
-      setCheckedIds(updatedIds);
-    }
-  };
 
   return (
     <div className={cx('wrapper')}>
@@ -72,7 +24,7 @@ function PrinterManagement() {
         </button>
       </div>
       <div className={cx('content')}>
-        <DisplayPrinter printers={printers} />
+        <DisplayPrinter />
       </div>
     </div>
   );
