b3326b1f760525922a50925a0ae56c11650a7798
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-17 23:49:38 +0700

    feat: add a generic Grid component

---
+++ frontend/src/components/Grid/Grid.jsx
@@ -0,0 +1,193 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import {
+  GridRowModes,
+  DataGrid,
+  GridToolbarContainer,
+  GridActionsCellItem,
+} from '@mui/x-data-grid';
+
+function EditToolbar({ setRows, setRowModesModel, checkedIds, createNewRow, deleteRows, columns }) {
+  const handleAddRows = async () => {
+    const newRow = await createNewRow();
+    setRows((oldRows) => [newRow, ...oldRows]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [newRow.id]: { mode: GridRowModes.Edit, fieldToFocus: columns[0].field },
+    }));
+  };
+
+  const handleDeleteRows = async () => {
+    await deleteRows(checkedIds);
+    setRows((oldRows) => oldRows.filter((row) => !checkedIds.includes(row.id)));
+    setRowModesModel({});
+  };
+
+  return (
+    <div className="flex">
+      <GridToolbarContainer>
+        <Button color="primary" startIcon={<AddIcon />} onClick={handleAddRows}>
+          Thêm máy in
+        </Button>
+      </GridToolbarContainer>
+      <GridToolbarContainer>
+        <Button color="primary" startIcon={<RemoveCircleOutlineIcon />} onClick={handleDeleteRows}>
+          Xoá máy in
+        </Button>
+      </GridToolbarContainer>
+    </div>
+  );
+}
+
+export default function Grid({
+  columns,
+  rows,
+  setRows,
+  loadRows,
+  deleteRows,
+  updateRows,
+  createNewRow,
+  showActions,
+}) {
+  const [rowModesModel, setRowModesModel] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+  const [checkedIds, setCheckedIds] = React.useState([]);
+
+  React.useEffect(() => {
+    loadRows().then(setRows).then(() => setLoading(false));
+  }, []);
+
+  const handleEditClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+    deleteRows([id]);
+  };
+
+  const handleCancelClick = (id) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+  };
+
+  const augmentedColumns = !showActions ? columns : [
+    ...columns,
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: 'primary.main',
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
+  const handleRowModesModelChange = (newRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const processRowUpdate = (updatedRow) => {
+    updateRows([updatedRow]);
+    setRows((oldRows) => {
+      const newRows = [...oldRows];
+      const updatedRowId = newRows.findIndex((row) => row.id === updatedRow.id);
+      if (updatedRowId >= 0) {
+        newRows[updatedRowId] = updatedRow;
+      }
+      return newRows;
+    });
+    return updatedRow;
+  };
+
+  return (
+    <Box
+      sx={{
+        height: 500,
+        width: '100%',
+        '& .actions': {
+          color: 'text.secondary',
+        },
+        '& .textPrimary': {
+          color: 'text.primary',
+        },
+      }}
+    >
+      <DataGrid
+        loading={loading}
+        rows={rows}
+        columns={augmentedColumns}
+        editMode="row"
+        checkboxSelection
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        processRowUpdate={processRowUpdate}
+        onRowSelectionModelChange={setCheckedIds}
+        slots={{
+          toolbar: EditToolbar,
+        }}
+        slotProps={{
+          toolbar: {
+            setRows,
+            setRowModesModel,
+            checkedIds,
+            createNewRow,
+            deleteRows,
+            columns: augmentedColumns,
+          },
+        }}
+      />
+    </Box>
+  );
+}
--- frontend/src/components/PrinterGrid/PrinterGrid.jsx
+++ frontend/src/components/PrinterGrid/PrinterGrid.jsx
@@ -1,241 +1,82 @@
 import * as React from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Close';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import {
-  GridRowModes,
-  DataGrid,
-  GridToolbarContainer,
-  GridActionsCellItem,
-} from '@mui/x-data-grid';
-import {
-  CREATE_PRINTERS_URL,
   DELETE_PRINTERS_URL,
+  GEN_PRINTER_URL,
   GET_PRINTERS_URL,
   UPDATE_PRINTERS_URL,
 } from '../../../constants/url';
+import Grid from '../Grid/Grid';
 
-function EditToolbar(props) {
-  const { setPrinters, setRowModesModel, checkIDs, setCheckIDs } = props;
-  const handleAddPrinters = () => {
-    const newPrinter = {
-      id: uuidv4(),
-      name: '',
-      location: '',
-      enabled: true,
-    };
-    setPrinters((oldRows) => [newPrinter, ...oldRows]);
-    setRowModesModel((oldModel) => ({
-      ...oldModel,
-      [newPrinter.id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
-    }));
-  };
-
-  const handleDeletePrinters = () => {
-    console.log(checkIDs);
-    axios({
-      method: 'post',
-      url: DELETE_PRINTERS_URL,
-      data: checkIDs,
-    })
-      .then((response) => {
-        console.log('Printers deleted successfully', response.data);
-        setPrinters((oldRows) => oldRows.filter((row) => !checkIDs.includes(row.id)));
-        setRowModesModel({});
-      })
-      .catch((error) => {
-        console.error('Error deleting printers', error);
-      });
-  };
-
-  return (
-    <div style={{ display: 'flex' }}>
-      <GridToolbarContainer>
-        <Button color="primary" startIcon={<AddIcon />} onClick={handleAddPrinters}>
-          Thêm máy in
-        </Button>
-      </GridToolbarContainer>
-      <GridToolbarContainer>
-        <Button color="primary" startIcon={<RemoveCircleOutlineIcon />} onClick={handleDeletePrinters}>
-          Xoá máy in
-        </Button>
-      </GridToolbarContainer>
-    </div>
-  );
-}
-
-export default function FullFeaturedCrudGrid() {
+export default function PrinterGrid() {
   const [printers, setPrinters] = React.useState([]);
-  const [rowModesModel, setRowModesModel] = React.useState({});
-  const [loading, setLoading] = React.useState(true);
-  const [checkIDs, setCheckIDs] = React.useState([]);
-
-  React.useEffect(async () => {
-    await axios.get(GET_PRINTERS_URL)
-      .then((response) => {
-        setPrinters(Object.values(response.data)[1]);
-      });
-    setLoading(false);
-  }, []);
-
-  const handleEditClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-  };
-
-  const handleSaveClick = (id) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-  };
-
-  const handleDeleteClick = (id) => () => {
-    setPrinters(printers.filter((printer) => printer.id !== id));
-    axios.post(DELETE_PRINTERS_URL, [id]);
-  };
-
-  const handleCancelClick = (id) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true },
-    });
-  };
-
-  const handleRowModesModelChange = (newRowModesModel) => {
-    setRowModesModel(newRowModesModel);
-  };
-
-  const handleRowEditStop = ({ row }) => {
-    axios.post(CREATE_PRINTERS_URL, [row]);
-  };
-
-  const processRowUpdate = (updatedRow) => {
-    axios.post(UPDATE_PRINTERS_URL, [updatedRow]);
-    setPrinters((oldPrinters) => {
-      const newPrinters = [...oldPrinters];
-      const updatedPrinterId = newPrinters.findIndex((printer) => printer.id === updatedRow.id);
-      if (updatedPrinterId>= 0) {
-        newPrinters[updatedPrinterId] = updatedRow;
-      }
-      return newPrinters;
-    });
-    return updatedRow;
-  };
-
+  const loadPrinters = () => axios.get(GET_PRINTERS_URL)
+    .then((response) => Object.values(response.data)[1]);
+  const deletePrinters = (ids) => axios.post(DELETE_PRINTERS_URL, ids);
+  const updatePrinters = (rows) => axios.post(UPDATE_PRINTERS_URL, rows);
+  const createNewPrinter = () => axios.post(GEN_PRINTER_URL)
+    .then((response) => Object.values(response.data)[1]);
+  const showActions = true;
   const columns = [
     {
       field: 'code',
       headerName: 'Code',
+    },
+    {
+      field: 'brand',
+      headerName: 'Brand',
       editable: true,
     },
     {
       field: 'name',
       headerName: 'Name',
-      width: 280,
       editable: true,
     },
     {
-      field: 'location',
-      headerName: 'Location',
-      type: 'text',
-      width: 280,
-      align: 'left',
-      headerAlign: 'left',
+      field: 'campus',
+      headerName: 'Campus',
+      type: 'singleSelect',
+      valueOptions: ['BK1', 'BK2'],
+      editable: true,
+    },
+    {
+      field: 'building',
+      headerName: 'Building',
+      type: 'singleSelect',
+      valueOptions: ['H1', 'H2', 'H3', 'H6', 'A1', 'A2', 'B1', 'B2', 'C1', 'C2'],
+      editable: true,
+    },
+    {
+      field: 'room',
+      headerName: 'Room',
+      type: 'number',
       editable: true,
     },
     {
       field: 'enabled',
       headerName: 'Enabled',
-      width: 180,
       editable: true,
       type: 'singleSelect',
       valueOptions: ['true', 'false'],
     },
     {
-      field: 'actions',
-      type: 'actions',
-      headerName: 'Actions',
-      width: 100,
-      cellClassName: 'actions',
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              icon={<SaveIcon />}
-              label="Save"
-              sx={{
-                color: 'primary.main',
-              }}
-              onClick={handleSaveClick(id)}
-            />,
-            <GridActionsCellItem
-              icon={<CancelIcon />}
-              label="Cancel"
-              className="textPrimary"
-              onClick={handleCancelClick(id)}
-              color="inherit"
-            />,
-          ];
-        }
-
-        return [
-          <GridActionsCellItem
-            icon={<EditIcon />}
-            label="Edit"
-            className="textPrimary"
-            onClick={handleEditClick(id)}
-            color="inherit"
-          />,
-          <GridActionsCellItem
-            icon={<DeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
+      field: 'description',
+      headerName: 'Desc.',
+      editable: true,
+      type: 'text',
     },
   ];
 
   return (
-    <Box
-      sx={{
-        height: 500,
-        width: '100%',
-        '& .actions': {
-          color: 'text.secondary',
-        },
-        '& .textPrimary': {
-          color: 'text.primary',
-        },
-      }}
- >
-      <DataGrid
-        loading={loading}
-        rows={printers}
-        columns={columns}
-        editMode="row"
-        checkboxSelection
-        rowModesModel={rowModesModel}
-        onRowModesModelChange={handleRowModesModelChange}
-        onRowEditStop={handleRowEditStop}
-        processRowUpdate={processRowUpdate}
-        onRowSelectionModelChange={(ids) => {
-          setCheckIDs(ids)
-        }}
-        slots={{
-          toolbar: EditToolbar,
-        }}
-        slotProps={{
-          toolbar: { setPrinters, setRowModesModel, checkIDs, setCheckIDs },
-        }}
-      />
-    </Box>
+    <Grid
+      columns={columns}
+      createNewRow={createNewPrinter}
+      deleteRows={deletePrinters}
+      loadRows={loadPrinters}
+      rows={printers}
+      setRows={setPrinters}
+      showActions={showActions}
+      updateRows={updatePrinters}
+    />
   );
 }
