0d44a88b8c667a5911f251326a09b1d2a7d27ad2
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-18 00:02:44 +0700

    feat: better heading

--- frontend/src/pages/printers/PrintersPage.jsx
+++ frontend/src/pages/printers/PrintersPage.jsx
@@ -4,9 +4,9 @@ import DisplayPrinter from '../../components/PrinterGrid/PrinterGrid';
 function PrintersPage() {
   return (
     <div>
-      <h2>
+      <h1 className="text-blue-900 my-6 font-bold text-3xl">
         QUẢN LÍ MÁY IN
-      </h2>
+      </h1>
       <div>
         <DisplayPrinter />
       </div>
