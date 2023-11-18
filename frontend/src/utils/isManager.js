5000fbe5aa422e6d6df21cfcc1b6c54731563314
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-19 01:27:00 +0700

    fix(fe): bug in isManager

--- frontend/src/utils/isManager.js
+++ frontend/src/utils/isManager.js
@@ -5,5 +5,5 @@ export default function isManager() {
   const cookiesString = document.cookie;
   const cookies = cookiesString.split(';').map((cookieString) => cookieString.trim().split('='));
   const isManagerCookie = cookies.find((cookie) => cookie[0] === 'isManager')?.at(1);
-  return Boolean(isManagerCookie);
+  return isManagerCookie === 'true';
 }
