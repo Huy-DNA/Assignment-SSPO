aa6bb76f2c496c72e9ca9f84e88efc44d4aded7b
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-19 08:58:37 +0700

    feat: rudimentary home page

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      Home page
+    <div className="h-full w-full">
+      <h1 className="text-center m-auto font-semibold text-3xl"> Hi 👋, BKU lovers</h1>
+      <p className="text-center m-auto text-lg">Enjoy our printing service!</p>
     </div>
   );
 }
