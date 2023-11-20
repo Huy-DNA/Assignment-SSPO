0340866fc59626737668283c7cc39690cdef60f2
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-20 21:05:47 +0700

    feat: add account popover

--- frontend/src/Layout/DefaultLayout/Header/Header.jsx
+++ frontend/src/Layout/DefaultLayout/Header/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Popover } from '@mui/material';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import images from '../../../../assets/images/images';
 import '../../../../styles.css';
@@ -68,10 +69,20 @@ function Header() {
     ? MANAGER_ITEMS
     : USER_ITEMS;
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleAccountIconClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     loginStatus !== LoginStatus.NOT_LOGGED_IN ? (
       <div className="flex flex-row items-center justify-between bg-slate-100 px-5">
-        <div className="w-16 h-16 m-4">
+        <div className="w-16 h-16 mx-10">
           <Link to="/">
             <img src={images.logo} alt="logo-web" />
           </Link>
@@ -101,12 +112,26 @@ function Header() {
             alt="user avatar"
             width={40}
             height={40}
+            onClick={handleAccountIconClick}
           />
+          <Popover
+            open={!!anchorEl}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+          >
+            <div className="p-3 rounded-md">
+              <p className="hover:bg-slate-200 p-2 rounded-md"><a href="/logout">Đăng xuất</a></p>
+            </div>
+          </Popover>
         </div>
       </div>
     ) : (
       <div className="flex flex-row items-center justify-between bg-slate-100 px-5">
-        <div className="w-16 h-16 m-4">
+        <div className="w-16 h-16 mx-10">
           <img src={images.logo} alt="logo-web" />
         </div>
 
