6052ce4ad223c34979a1fd69234509577b4a7d9c
havanchau <chauhvc2.dy@gmail.com>
2023-11-20 09:27:18 +0700

    change css

--- frontend/src/pages/printfile/PrintFile.jsx
+++ frontend/src/pages/printfile/PrintFile.jsx
@@ -5,17 +5,74 @@ function PrintFile() {
   return (
     <div className="w-full">
       <h1 className="text-3xl font-medium pl-80 pb-4">IN TÀI LIỆU</h1>
-      <div className="w-[50rem] h-[40rem] mx-auto bg-blue-300 rounded-3xl">
-        <div className="w-full flex justify-start items-center flex-col mt-1 mb-3">
-          <label className="text-xs font-semibold mb-1">Vai trò</label>
+      <div className="w-[50rem] h-[40rem] mx-auto bg-blue-200 rounded-3xl">
+        <div className="w-full flex justify-between">
+          <div className="w-4/6 flex justify-start flex-col mt-4 mb-3 ml-8">
+            <div>
+              <label className="font-medium mb-1 text-xl">Chọn file để in</label>
+              <select
+                id="role"
+                className="input min-w-[30rem] min-h-[2.5rem] text-sm text-slate-500 rounded-lg"
+                style={{ fontSize: '18px', padding: '6px' }}
+              >
+                <option style={{ fontSize: '18px', padding: '6px' }}>Chọn file đã tải lên</option>
+                <option value="DSA" style={{ fontSize: '18px', padding: '6px' }}>Cấu trúc dl&gt</option>
+                <option value="MD" style={{ fontSize: '18px', padding: '6px' }}>Mô hình hóa toán học</option>
+              </select>
+            </div>
+          </div>
+          <div className="w-1/5 h-[41px] text-xl font-medium px-8 py-2 mt-[44px] bg-white mr-8 rounded-lg hover:cursor-pointer items-center hover:bg-gray-200">
+            <button>Tải file lên</button>
+          </div>
+        </div>
+        <div className="h-28 bg-white mx-8 my-8 rounded-lg"></div>
+        <div className="mx-8">
+          <label className="font-medium mb-1 text-xl">Chọn khổ giấy in in</label>
           <select
             id="role"
-            className="input min-w-[30rem] min-h-[2.5rem] text-sm text-slate-500"
+            className="input w-full min-h-[2.5rem] text-sm text-slate-500 rounded-lg"
+            style={{ fontSize: '18px', padding: '6px' }}
           >
-            <option>Chọn file đã tải lên</option>
-            <option value="DSA">Cấu trúc dl&gt</option>
-            <option value="MD">Mô hình hóa toán học</option>
+            <option style={{ fontSize: '18px', padding: '6px' }}>Chọn khổ giấy in</option>
+            <option value="DSA" style={{ fontSize: '18px', padding: '6px' }}>A4</option>
+            <option value="MD" style={{ fontSize: '18px', padding: '6px' }}>A3</option>
           </select>
+        </div>
+        <div className="mx-8 flex justify-between mt-8">
+          <div>
+            <div className="font-medium mb-1 text-xl">Số trang giấy in</div>
+            <div className="w-3/8 h-[41px] bg-white text-xl px-20 py-2 rounded-lg">
+              100 trang
+            </div>
+          </div>
+          <div>
+          <div className="font-medium mb-1 text-xl">Số trang giấy in còn lại</div>
+            <div className="w-3/8 h-[41px] bg-white text-xl px-20 py-2 rounded-lg">
+              200 trang
+            </div>
+          </div>
+        </div>
+        <div className="mx-8 mt-8">
+          <label className="font-medium mb-1 text-xl">Chọn máy in</label>
+          <select
+            id="role"
+            className="input w-full min-h-[2.5rem] text-sm text-slate-500 rounded-lg"
+            style={{ fontSize: '18px', padding: '6px' }}
+          >
+            <option style={{ fontSize: '18px', padding: '6px' }}>Chọn máy in</option>
+            <option value="Printer1" style={{ fontSize: '18px', padding: '6px' }}>Máy in 1 - H6-219</option>
+            <option value="Printer2" style={{ fontSize: '18px', padding: '6px' }}>Máy in 2 - H2-123</option>
+            <option value="Printer3" style={{ fontSize: '18px', padding: '6px' }}>Máy in 3 - H6-212</option>
+            <option value="Printer4" style={{ fontSize: '18px', padding: '6px' }}>Máy in 4 - H2-122</option>
+          </select>
+        </div>
+        <div className="mx-8 mt-8 flex justify-between">
+          <button className="w-32 h-10 bg-red-500 hover:cursor-pointer hover:bg-red-800 text-white text-xl rounded-lg">
+            Hủy
+          </button>
+          <button className="w-32 h-10 bg-blue-600 hover:cursor:pointer hover:bg-blue-800 text-xl rounded-lg">
+            Xác nhận
+          </button>
         </div>
       </div>
     </div>
