487bb17fbbd1e88a30cd4aa83ad6352bb0bc5c13
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-11-28 20:15:08 +0700

    feat: support waiting notification

--- frontend/src/components/Notification/NotificationBox.jsx
+++ frontend/src/components/Notification/NotificationBox.jsx
@@ -6,11 +6,21 @@ import { faClose } from "@fortawesome/free-solid-svg-icons";
 export default function NotificationBox({ status, message, onClose }) {
   return (
     <div className={
-      `${status === NotificationStatus.OK ? "bg-green-200 border-green-300 text-green-500" : "bg-red-200 border-red-300 text-red-500"} ` +
+      `${status === NotificationStatus.OK ? "bg-green-200 border-green-300 text-green-500 " :
+         status === NotificationStatus.ERR ? "bg-red-200 border-red-300 text-red-500 " :
+         status === NotificationStatus.WAITING ? "bg-blue-200 border-blue-300 text-blue-300 " :
+         " "} ` +
       "rounded-md border-2 w-[90vw] sm:w-[33vw] fixed right-5 top-5 p-2 z-50"
     }>
       <button onClick={onClose} className="absolute right-2"><FontAwesomeIcon icon={faClose}/></button>
-      <p className="font-bold">{ status === NotificationStatus.OK ? "Successful" : "Error" }</p>
+      <p className="font-bold">
+        { 
+          status === NotificationStatus.OK ? "Successful" :
+          status === NotificationStatus.ERR ? "Error" :
+          status === NotificationStatus.WAITING ? "Processing" :
+          " "
+        }
+      </p>
       <p>{ message }</p>
     </div>
   )
