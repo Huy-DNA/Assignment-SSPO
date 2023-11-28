210a691ed5fd739a0d6acd413cab1f8ca0195bed
havanchau <chauhvc2.dy@gmail.com>
2023-11-28 11:20:17 +0700

    add update

--- frontend/src/pages/printfile/PrintFile.jsx
+++ frontend/src/pages/printfile/PrintFile.jsx
@@ -13,17 +13,31 @@ function PrintFile() {
     numberPage: '',
     idPrinter: '',
   });
-  console.log(fileToPrint);
+  const [printers, setPrinters] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/files').then((data) => {
-      console.log(data)
-      setFiles(data.data.value);
+    const getFiles = async () => axios.get('http://localhost:3000/api/files').then((response) => {
+      setFiles(response.data.value);
     })
       .catch(error => {
         console.log(error)
       });
+
+    getFiles();
   }, []);
+
+  useEffect(() => {
+    const getPrinters = async () => axios.get('http://localhost:3000/api/printers').then((response) => {
+      setPrinters(response.data.value);
+      console.log(response.data.value);
+    })
+      .catch(error => {
+        console.log(error)
+      });
+
+    getPrinters();
+
+  }, [])
 
   const getInfoFile = (fileName) => {
     return files.findIndex((value) => value.name === fileName);
@@ -77,24 +91,39 @@ function PrintFile() {
           }
         </div>
         <div className="mx-8">
-          <label className="font-medium mb-1 text-xl">Chọn khổ giấy in in</label>
-          <select
-            id="role"
-            className="input w-full min-h-[2.5rem] text-sm text-slate-500 rounded-lg"
-            style={{ fontSize: '18px', padding: '6px' }}
-            onChange={(e) => {
-              setFileToPrint({ ...fileToPrint, pageType: e.target.value})
-            }}
-          >
-            <option style={{ fontSize: '18px', padding: '6px' }}>Chọn khổ giấy in</option>
-            <option value="A4" style={{ fontSize: '18px', padding: '6px' }}>A4</option>
-            <option value="A3" style={{ fontSize: '18px', padding: '6px' }}>A3</option>
-          </select>
+          <div className="flex justify-between">
+            <select
+              id="role"
+              className="w-3/8 input w-full min-h-[2.5rem] text-sm text-slate-500 rounded-lg mr-8"
+              style={{ fontSize: '18px', padding: '6px' }}
+              onChange={(e) => {
+                setFileToPrint({ ...fileToPrint, pageType: e.target.value })
+              }}
+            >
+              <option style={{ fontSize: '18px', padding: '6px' }}>Chọn số bản in muốn in</option>
+              <option value="1" style={{ fontSize: '18px', padding: '6px' }}>1</option>
+              <option value="2" style={{ fontSize: '18px', padding: '6px' }}>2</option>
+              <option value="3" style={{ fontSize: '18px', padding: '6px' }}>3</option>
+              <option value="4" style={{ fontSize: '18px', padding: '6px' }}>4</option>
+            </select>
+            <select
+              id="role"
+              className="w-3/8 input w-full min-h-[2.5rem] text-sm text-slate-500 rounded-lg ml-8"
+              style={{ fontSize: '18px', padding: '6px' }}
+              onChange={(e) => {
+                setFileToPrint({ ...fileToPrint, pageType: e.target.value })
+              }}
+            >
+              <option style={{ fontSize: '18px', padding: '6px' }}>Chọn khổ giấy in</option>
+              <option value="A4" style={{ fontSize: '18px', padding: '6px' }}>A4</option>
+              <option value="A3" style={{ fontSize: '18px', padding: '6px' }}>A3</option>
+            </select>
+          </div>
         </div>
-        <div className="mx-8 flex justify-between mt-8">
-          <div>
-            <div className="font-medium mb-1 text-xl">Số trang giấy in</div>
-            <div className="w-3/8 h-[41px] bg-white text-xl px-20 py-2 rounded-lg">
+        <div className="mx-8 flex justify-between mt-4">
+          <div className="w-3/8">
+            <div className="w-full font-medium mb-1 text-xl">Số trang giấy in</div>
+            <div className="w-[337px] h-[41px] bg-white text-xl py-2 rounded-lg pl-32">
               {
                 fileToPrint.file && fileToPrint.pageType === 'A4' ? (
                   fileToPrint.numberPage
@@ -108,9 +137,9 @@ function PrintFile() {
               }
             </div>
           </div>
-          <div>
-            <div className="font-medium mb-1 text-xl">Số trang giấy in còn lại</div>
-            <div className="w-3/8 h-[41px] bg-white text-xl px-20 py-2 rounded-lg">
+          <div className="w-3/8">
+            <div className="w-full font-medium mb-1 text-xl">Số trang giấy in còn lại</div>
+            <div className="w-[337px] h-[41px] bg-white text-xl py-2 rounded-lg align-top pl-32">
               200 trang
             </div>
           </div>
@@ -123,10 +152,13 @@ function PrintFile() {
             style={{ fontSize: '18px', padding: '6px' }}
           >
             <option style={{ fontSize: '18px', padding: '6px' }}>Chọn máy in</option>
-            <option value="Printer1" style={{ fontSize: '18px', padding: '6px' }}>Máy in 1 - H6-219</option>
-            <option value="Printer2" style={{ fontSize: '18px', padding: '6px' }}>Máy in 2 - H2-123</option>
-            <option value="Printer3" style={{ fontSize: '18px', padding: '6px' }}>Máy in 3 - H6-212</option>
-            <option value="Printer4" style={{ fontSize: '18px', padding: '6px' }}>Máy in 4 - H2-122</option>
+            {
+              printers && printers.map((printer, index) => (
+                <option value={printer.id} key={printer.id} style={{ fontSize: '18px', padding: '6px' }}>
+                  {printer.campus} {printer.building} {printer.room}
+                </option>
+              ))
+            }
           </select>
         </div>
         <div className="mx-8 mt-8 flex justify-between">
