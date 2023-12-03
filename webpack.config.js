22398becf4830cf094dea6c1263dbaaa95287080
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-12-03 19:02:54 +0700

    chore: fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ export default [
     },
     mode: 'development',
     resolve: {
-      extensions: ['*', '.js', '.jsx'],
+      extensions: ['.*', '.js', '.jsx'],
     },
     module: {
       rules: [
