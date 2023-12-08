2f9b6ce5b52cbc51e1170da2f70dd3af60b5268b
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-12-08 13:00:13 +0700

    fix: make notification text more illegible

--- frontend/src/components/Notification/NotificationBox.jsx
+++ frontend/src/components/Notification/NotificationBox.jsx
@@ -13,9 +13,9 @@ import { NotificationStatus } from '../../constants/notification';
 export default function NotificationBox({ status, message, onClose }) {
   return (
     <div className={
-      `${status === NotificationStatus.OK ? 'bg-green-200 border-green-300 text-green-500 '
-        : status === NotificationStatus.ERR ? 'bg-red-200 border-red-300 text-red-500 '
-          : status === NotificationStatus.WAITING ? 'bg-blue-200 border-blue-300 text-blue-300 '
+      `${status === NotificationStatus.OK ? 'bg-green-200 border-green-300 text-green-800 '
+        : status === NotificationStatus.ERR ? 'bg-red-200 border-red-300 text-red-800 '
+          : status === NotificationStatus.WAITING ? 'bg-blue-200 border-blue-300 text-blue-800 '
             : ' '} `
       + 'rounded-md border-2 w-[90vw] sm:w-[33vw] fixed right-5 top-5 p-2 z-50'
     }
